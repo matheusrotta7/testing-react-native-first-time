db515ce8ac221633eda6e6ec1cc742245b0c421d
Matheus Rotta <matheusrotta7@gmail.com>
2023-10-05 22:35:12 -0300

    testing react native for the first time

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Calendar, CalendarCheck2 } from 'lucide';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Matheus was here!</Text>
+      <Text>This is freaking awesome</Text>
+      <Button title='Hello Awesome'></Button>
       <StatusBar style="auto" />
+      <View>
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: 'powderblue',
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: 'red',
+        }}
+      />
+    </View>
     </View>
   );
 }
